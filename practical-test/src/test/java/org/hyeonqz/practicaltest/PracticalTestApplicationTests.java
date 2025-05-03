2beea7fecefbce1e42d4d3b4bd32ec69f62a91fb
hkjin <wlsgusrb78@naver.com>
2025-05-03 15:09:18 +0900

    init Practical Testing

---
+++ practical-test/src/test/java/org/hyeonqz/practicaltest/PracticalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hyeonqz.practicaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalTestApplicationTests {
+
+	@Test
+	void contextLoads () {
+	}
+
+}
